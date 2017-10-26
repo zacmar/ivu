93d7738377cd291b9512424a613e4e313cbb84a1
Mustafa Mahmoud <m.mahmoud@student.tugraz.at>
2017-10-26 17:37:31 +0200

    color calc

--- video_stream.py
+++ video_stream.py
@@ -1,57 +1,40 @@
-import cv2
-import numpy as np
-
 ALL_CONTOURS = -1
 
 import cv2
+import numpy as np
 
-def th1Chng(x):
-    pass
-
-def th2Chng(x):
-    pass
-
-def epsChng(x):
-    pass
-
-wind = cv2.namedWindow("Canny Edge Threshold Differences", cv2.WINDOW_AUTOSIZE)
 cap = cv2.VideoCapture(0)
-th1 = cv2.createTrackbar('Threshold 1', "Canny Edge Threshold Differences", 0, 100, th1Chng)
-th2 = cv2.createTrackbar('Threshold 2', "Canny Edge Threshold Differences", 0, 100, th2Chng)
-th3 = cv2.createTrackbar('Epsilon', "Canny Edge Threshold Differences", 0, 1000, epsChng)
-cv2.setTrackbarPos('Threshold 1','Canny Edge Threshold Differences', 65)
-cv2.setTrackbarPos('Threshold 2','Canny Edge Threshold Differences', 85)
-cv2.setTrackbarPos('Epsilon','Canny Edge Threshold Differences', 60)
-while(True):
-    threshold1 = cv2.getTrackbarPos('Threshold 1','Canny Edge Threshold Differences')
-    threshold2 = cv2.getTrackbarPos('Threshold 2','Canny Edge Threshold Differences')
-    epstemp = cv2.getTrackbarPos('Epsilon','Canny Edge Threshold Differences')
 
+while(True):
     ret, frame = cap.read()
 
+    threshold1 = 50
+    threshold2 = 20
     gray = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), (3, 3), 0)
     im2 = cv2.Canny(gray,threshold1,threshold2)
     im2, contours, hierarchy = cv2.findContours(im2.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-
-    #convexContours = [contour for contour in contours if cv2.isContourConvex(contour) and cv2.contourArea(contour) > 1]
-
-    indizes = []
+    indizes= []
     for cnt in contours:
-        epsilon = epstemp/1000*cv2.arcLength(cnt,True)
+        epsilon = 0.06*cv2.arcLength(cnt,True)
         indizes.append(cv2.approxPolyDP(cnt,epsilon,True))
-
     cont = [contour for contour in indizes if cv2.isContourConvex(contour)
-            and cv2.contourArea(contour) > 400
-            and contour.shape[0] == 4]
-
-    #im2[::] = 0
-
-    test = cv2.drawContours(im2, cont, ALL_CONTOURS, (255,255,0), 5)
-    #videostreams = np.hstack((gray,test))
-    completewindow = np.vstack((videostreams, th1))
-
-    cv2.imshow("Canny Edge Threshold Differences", test)
-
+                                           and cv2.contourArea(contour) > 400
+                                           and contour.shape[0] == 4]
+    boundingRects = []
+    for contour in cont:
+        boundingRects.append(cv2.minAreaRect(contour))
+    im2[::] = 0
+    mask = im2
+    means = []
+    for boundingRect in boundingRects:
+        mask[::] = 0 # = im2
+        cv2.fillPoly(mask, np.int32([cv2.boxPoints(boundingRect)]), (255, 255, 255))
+        means.append(cv2.mean(frame, mask))
+    im2 = cv2.cvtColor(im2, cv2.COLOR_GRAY2RGB)
+    for index, contour in enumerate(cont):
+        cv2.drawContours(im2, [contour], ALL_CONTOURS, means[index], 5)
+    cv2.imshow('frame', im2)
+    cv2.imshow('grey', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
