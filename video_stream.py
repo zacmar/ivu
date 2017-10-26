9ddfe5a08d43bf0346e35d480578b1b65fda85d8
Martin Zach <martin.zach@student.tugraz.at>
2017-10-26 14:54:26 +0200

    checking contours to have exactly 4 corners

--- video_stream.py
+++ video_stream.py
@@ -14,14 +14,16 @@ while(True):
     im2 = cv2.Canny(gray,threshold1,threshold2)
     im2, contours, hierarchy = cv2.findContours(im2.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
     convexContours = [contour for contour in contours if cv2.isContourConvex(contour) and cv2.contourArea(contour) > 1]
-    indizes = []
+    indizes= []
     for cnt in contours:
         epsilon = 0.06*cv2.arcLength(cnt,True)
         indizes.append(cv2.approxPolyDP(cnt,epsilon,True))
     #cv2.drawContours(im2, contours, -1, (0, 255, 0), 3)
     cont = [contour for contour in indizes if cv2.isContourConvex(contour) and cv2.contourArea(contour) > 400]
-    print(len(contours))
     im2[::] = 0
+    for contour in cont:
+        print(contour.shape)
+    cont = [contour for contour in cont if contour.shape[0] == 4]
     cv2.drawContours(im2, cont, -1, (255,255,0), 5)
     cv2.imshow('frame', im2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
